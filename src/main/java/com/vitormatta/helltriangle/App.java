d3ccd720e29b0fbfe9195f6ea1d46722ae81f9d2
Vitor Matta <vitor.matta@webradar.com>
2017-05-14 18:08:22 -0300

    First test case and add log to gitignore

--- src/main/java/com/vitormatta/helltriangle/App.java
+++ src/main/java/com/vitormatta/helltriangle/App.java
@@ -1,17 +1,22 @@
 package com.vitormatta.helltriangle;
 
+import org.apache.log4j.Logger;
+
 /**
- * Hello world!
+ * Main class for the hell triangle challenge
+ * Author: Vitor Matta
+ * Email: vitormm@gmail.com
  *
  */
 public class App 
 {
+    final static Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
         if (args == null || args.length == 0)
             throw new IllegalArgumentException("You must supply at least one argument");
         if (args[0] == null)
             throw new IllegalArgumentException("You must supply at least one argument");
-
     }
 }
--- src/test/java/com/vitormatta/helltriangle/AppTest.java
+++ src/test/java/com/vitormatta/helltriangle/AppTest.java
@@ -1,38 +1,22 @@
 package com.vitormatta.helltriangle;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.vitormatta.helltriangle.App;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
- * Unit test for simple App.
+ * Unit test for Main Application class
  */
+@RunWith(JUnit4.class)
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testRunAppWithNoInputArgs() throws Throwable{
+        String[] args = null;
+        App.main( args );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
