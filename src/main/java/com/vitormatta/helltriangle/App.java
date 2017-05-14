1d3c0e556598f20a7dd38a40860768f97479b3a6
Vitor Matta <vitor.matta@webradar.com>
2017-05-14 01:07:50 -0300

    Change maven config to generate jar with dependencies and javadoc etc.

--- src/main/java/com/vitormatta/helltriangle/App.java
+++ src/main/java/com/vitormatta/helltriangle/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args == null || args.length == 0)
+            throw new IllegalArgumentException("You must supply at least one argument");
+        if (args[0] == null)
+            throw new IllegalArgumentException("You must supply at least one argument");
+
     }
 }
